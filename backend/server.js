e06950ead61514fd347c00ef1396bd0deee9e2e2
sauravraj45 <sauravraj9525498862@gmail.com>
2026-05-27 10:15:36 +0530

    updated cors for netlify

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,3 @@
-
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
@@ -9,15 +8,20 @@ require("./db");
 
 // middleware
 app.use(express.json());
-app.use(cors());
 
-
+app.use(cors({
+  origin: "https://assignment-74-1.netlify.app",
+  credentials: true
+}));
 
 // routes
 app.use("/api/payment", require("./paymentRoutes"));
-app.use("/api/auth", require("./authRoutes"));  
-app.use("/api/address", require("./addressRoutes"));   
+app.use("/api/auth", require("./authRoutes"));
+app.use("/api/address", require("./addressRoutes"));
 app.use("/api/order", require("./orderRoutes"));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+
+app.listen(PORT, () =>
+  console.log(`Server running on ${PORT}`)
+);
